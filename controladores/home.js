b0cbed9b921ec34f88fbb1737f5fb259c9052521
Martin <mpforlani@gmail.com>
2022-02-18 23:26:29 -0300

    123

--- controladores/home.js
+++ controladores/home.js
@@ -10,7 +10,6 @@ const { unlink } = require(`fs-extra`)
 const User = require("../models/marketPlace/User");
 const Error = require("../models/home/Error");
 const Requerimiento = require("../models/home/Requerimiento");
-
 const Tarea = require("../models/marketPlace/procesos/Tarea");
 const Estado = require("../models/marketPlace/procesos/Estado");
 const Criticidad = require("../models/marketPlace/procesos/Criticidad");
